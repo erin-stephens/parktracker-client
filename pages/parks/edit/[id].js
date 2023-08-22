0aa037d9b1419ba0b228f819c0c7639dd33fce48
Erin Stephens <erebstephens@gmail.com>
2023-08-21 21:23:13 -0500

    created park form, added to new and edit pages, also delete works

--- pages/parks/edit/[id].js
+++ pages/parks/edit/[id].js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { getSinglePark } from '../../../utils/data/parkData';
+import ParkForm from '../../../components/park/ParkForm';
 
 export default function EditPark() {
+  const [editPark, setEditPark] = useState({});
+  const router = useRouter();
+  const { id } = router.query;
+
+  useEffect(() => {
+    getSinglePark(id).then(setEditPark);
+  }, [id]);
+
   return (
-    <div>[id]</div>
+    <ParkForm obj={editPark} />
   );
 }
