6bbda23d4a29b81a1cdb9e754a9e31ad119e4d9a
Erin Stephens <erebstephens@gmail.com>
2023-08-22 19:33:18 -0500

    added trail form, can full CRUD on trails

--- pages/trails/new.js
+++ pages/trails/new.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import TrailForm from '../../components/trail/TrailForm';
+import { useAuth } from '../../utils/context/authContext';
 
 export default function CreateTrailPage() {
+  const { user } = useAuth();
   return (
-    <div>CreateTrailPage</div>
+    <div>
+      <h1>Add a New Trail</h1>
+      <TrailForm user={user} />
+    </div>
   );
 }
