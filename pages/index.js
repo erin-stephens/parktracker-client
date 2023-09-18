107fe7cd75469eb0827a543b81f098884245b9ae
Erin Stephens <erebstephens@gmail.com>
2023-09-17 21:22:43 -0500

    continued edits to homepage

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,8 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import parkhome from '../src/assets/pics/parkhome.jpeg';
+import sitehome from '../src/assets/pics/sitehome.jpeg';
+import trailhome from '../src/assets/pics/trailhome.jpeg';
 import Header from '../components/Header';
 
 function Home() {
@@ -7,7 +12,7 @@ function Home() {
         <div
           className="header"
           style={{
-            height: '160px',
+            height: '200px',
             width: '200px',
           }}
         >
@@ -16,19 +21,58 @@ function Home() {
       </div>
       <div className="hometabs">
         <div className="parkhome">
-          <h1>Parks</h1>
-          <div> park image</div>
-          <div> park info</div>
+          <div
+            style={{
+              height: '200px',
+              width: '200px',
+            }}
+            className="imagecontainer"
+          >
+            <Image src={parkhome} alt="parkimage" className="parkimage" />
+          </div>
+          <div className="textcontainer">
+            <h1>Parks</h1>
+            <div> park info</div>
+            <Link passHref href="/parks">
+              <button type="button"> Go To Parks </button>
+            </Link>
+          </div>
         </div>
         <div className="trailhome">
-          <h1>Trails</h1>
-          <div> trail image</div>
-          <div> trail info</div>
+          <div
+            style={{
+              height: '200px',
+              width: '200px',
+            }}
+            className="imagecontainer"
+          >
+            <Image src={sitehome} alt="siteimage" className="siteimage" />
+          </div>
+          <div className="textcontainer">
+            <h1>Trails</h1>
+            <div> trail info</div>
+            <Link passHref href="/trails">
+              <button type="button"> Go To Trails </button>
+            </Link>
+          </div>
         </div>
         <div className="sitehome">
-          <h1>Sites</h1>
-          <div> site image</div>
-          <div> site info</div>
+          <div
+            style={{
+              height: '160px',
+              width: '200px',
+            }}
+            className="imagecontainer"
+          >
+            <Image src={trailhome} alt="trailimage" className="trailimage" />
+          </div>
+          <div className="textcontainer">
+            <h1>Sites</h1>
+            <div> site info</div>
+            <Link passHref href="/sites">
+              <button type="button"> Go To Sites </button>
+            </Link>
+          </div>
         </div>
       </div>
     </>
